83a385770c929c129134a577e98a659ffd072e3a
ali <aa5325368@gmail.com>
2022-09-30 21:56:36 +0200

    u

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -328,13 +328,15 @@ const List = ({ mode }) => {
                             })
                             setposts(postlists);
                         } else {
-                            setnofr(<span className=' text-xl dark:text-white'>You have no friends</span>)
+                            setnofr(<span className=' text-xl dark:text-white'>You have no saved items</span>)
         
                         }
                     }else {
-                        setnofr(<span className=' text-xl dark:text-white'>You have no friends</span>)
+                        setnofr(<span className=' text-xl dark:text-white'>You have no saved items</span>)
     
                     }
+                }else{
+                    setnofr(<span className=' text-xl dark:text-white'>You have no saved items</span>)
                 }
             }
             else {
