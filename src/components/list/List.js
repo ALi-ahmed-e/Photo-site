10bf541c444c3fc27da3fcd05632acc5650b59c6
ALi-ahmed-e <107031226+ALi-ahmed-e@users.noreply.github.com>
2022-11-05 04:36:55 +0200

    fixes

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -833,10 +833,10 @@ const List = ({ mode }) => {
 
 
 
-            <div className={`z-10 fixed top-0 bottom-0 left-0 right-0 bg-glass flex  justify-center`} style={editpostl ? { 'display': 'flex' } : { 'display': 'none' }}>
+            <div onClick={(e) => e.target.id != 'not'&&seteditpostl()} className={`z-10 fixed top-0 bottom-0 left-0 right-0 bg-glass flex  justify-center`} style={editpostl ? { 'display': 'flex' } : { 'display': 'none' }}>
 
-                <div className=' mt-14 h-fit pb-5 z-40 w-[90%] max-w-[600px]  bg-white dark:bg-black  rounded-2xl  '>
-                    <XMarkIcon onClick={() => { seteditpostl() }} className=' mx-2 my-2 w-6 h-6 dark:text-white cursor-pointer' />
+                <div id='not' className=' mt-14 h-fit pb-5 z-40 w-[90%] max-w-[600px]  bg-white dark:bg-black  rounded-2xl  '>
+                    <XMarkIcon id='yes' className=' mx-2 my-2 w-6 h-6 dark:text-white cursor-pointer' />
 
                     <div className='h-full flex flex-col items-center  w-full ml-1'>
                         <div className=' self-start ml-3 mt-5 flex  h-fit items-center'>
